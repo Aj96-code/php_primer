9bbd636717ea33fcc31b89a82cc7632f69768539
Aj96-code <69065112+Aj96-code@users.noreply.github.com>
2022-09-15 21:54:54 -0500

    Close header tags

--- IfStatement.php
+++ IfStatement.php
@@ -11,12 +11,12 @@
         if($gradeInt >= 50)
         {
             echo 
-                "<h3 style='color: green;'>YOU HAVE PASSED<h3>";
+                "<h3 style='color: green;'>YOU HAVE PASSED</h3>";
         }
         else
         {
             echo 
-                "<h3 style='color: red;'>YOU HAVE FAILED<h3>";
+                "<h3 style='color: red;'>YOU HAVE FAILED</h3>";
 
         }
         $gradeChar = "F";
@@ -24,18 +24,18 @@
         {
 
             echo 
-                "<h2 style='color: green;'>YOU ARE A SUPERSTAR!<h2>";
+                "<h2 style='color: green;'>YOU ARE A SUPERSTAR!</h2>";
         }
         else if ($gradeChar == "B")
         {
 
             echo 
-                "<h2 style='color: blue;'>YOU DID WELL!<h2>";
+                "<h2 style='color: blue;'>YOU DID WELL!</h2>";
         } else 
         {
             
             echo 
-                "<h2 style='color: red;'>YOU HAVE FAILED<h2>";
+                "<h2 style='color: red;'>YOU HAVE FAILED</h2>";
         }
     ?>
 <?php require ("./Includes/footer.php");?>
--- SwitchStatement.php
+++ SwitchStatement.php
@@ -10,19 +10,19 @@
                 echo 
                     "<h2 style= 'color: green;'>"
                         ."YOU ARE A SUPERSTAR!".
-                        "<h2>";
+                        "</h2>";
                 break;
             case "B":
                 echo 
                     "<h2 style= 'color: blue;'>"
                         ."YOU DID WELL".
-                        "<h2>";
+                        "</h2>";
                 break;
             default:
                 echo 
                     "<h2 style= 'color: red;'>"
                         ."YOU HAVE FAILED".
-                        "<h2>";
+                        "</h2>";
                 break;
                 
         }
