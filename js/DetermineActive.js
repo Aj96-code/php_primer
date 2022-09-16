08534a150db57a093175557c2fe0224c8f24f390
Aj96-code <69065112+Aj96-code@users.noreply.github.com>
2022-09-15 22:01:52 -0500

    fix if statement for active class

--- js/DetermineActive.js
+++ js/DetermineActive.js
@@ -1,7 +1,13 @@
 const activePage = window.location.pathname;
 const navLinks = document.querySelectorAll("ul li a").
 forEach(link =>{
-    if(link.href.includes(`${activePage}`)){
+    if(activePage === "/")
+    {
+        if(link.href === "/index.php"){
+            link.classList.add("active");
+        }
+    }
+    else if(link.href.includes(`${activePage}`)){
         link.classList.add("active");
     }
 });
